2de46a8bc3200ed98c8f5c1473125972b0056902
aransword <140793632+aransword@users.noreply.github.com>
2026-01-08 17:41:05 +0900

    [Bronze II] Title: 소수 찾기, Time: 180 ms, Memory: 17844 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+
+            if(a % 2 == 1 && a != 1) {
+                boolean prime = true;
+
+                for(int x = 2; x <= a / 2; x++) {
+                    if(a % x == 0) {
+                        prime = false;
+                        break;
+                    }
+                }
+
+                if(prime) {
+                    result++;
+                }
+            } else if (a == 2) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
