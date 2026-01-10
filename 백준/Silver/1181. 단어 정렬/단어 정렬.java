9d89584d18fab1269ab498bb8331f6ba53cee957
김동욱 <140793632+aransword@users.noreply.github.com>
2026-01-11 03:56:53 +0900

    [Silver V] Title: 단어 정렬, Time: 696 ms, Memory: 37736 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String[] words = new String[N];
+
+        for(int i = 0; i < N; i++) {
+            words[i] = sc.next();
+        }
+
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() != s2.length()) {
+                    return s1.length() - s2.length();
+                }
+                return s1.compareTo(s2);
+            }
+        });
+
+        System.out.println(words[0]);
+        for(int i = 1; i < N; i++) {
+            if(!words[i].equals(words[i - 1]))
+                System.out.println(words[i]);
+        }
+
+        sc.close();
+    }
+}
